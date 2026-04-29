e1b3ee1e0bda6af5e9f928dfb3bdfa54fc3f4936
PhilemonVanDussen <vandussenph709@northwested.org>
2026-04-29 13:27:35 -0400

    update

--- starter.js
+++ starter.js
@@ -1,9 +1,20 @@
-// First and last name 
-// Current date
+// Philemon VanDussen 
+// 4/29/2026
 // JS Image Swap Project
 
+const modelImages = ['girl-with-chopsticks.jpg', 'infinity-trail.jpg'];
+const imgElement = document.getElementById('model-frame');
 
+const preload = new Image();
+preload.src = modelImages[1];
 
+imgElement.addEventListener('mouseenter', () => {
+    imgElement.src = modelImages[1];
+})
+
+imgElement.addEventListener('mouseleave', () => {
+    imgElement.src = modelImages[0]
+})
 
 
 
